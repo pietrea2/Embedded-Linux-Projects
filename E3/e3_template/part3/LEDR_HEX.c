8d6c8ddd0c09ee5617c7790da6e58d552b80d145
Adam Pietrewicz <adam.pietrewicz@mail.utoronto.ca>
2024-10-03 15:20:32 -0400

    Added submission template for E3

---
+++ E3/e3_template/part3/LEDR_HEX.c
@@ -0,0 +1,10 @@
+#include <linux/fs.h>               // struct file, struct file_operations
+#include <linux/init.h>             // for __init, see code
+#include <linux/module.h>           // for module init and exit macros
+#include <linux/miscdevice.h>       // for misc_device_register and struct miscdev
+#include <linux/uaccess.h>          // for copy_to_user, see code
+#include <asm/io.h>                 // for mmap
+#include "../include/address_map_arm_vm.h"
+
+
+/**  your part 3 kernel code here  **/
