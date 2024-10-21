64a504ab5ce5342d6c6fc8473566e4be631e89ac
Adam Pietrewicz <adam.pietrewicz@mail.utoronto.ca>
2024-10-21 02:20:43 -0400

    Cleaned and tested part1 stopwatch char dev driver

--- E4/e4_template/part1/stopwatch.c
+++ E4/e4_template/part1/stopwatch.c
@@ -9,53 +9,17 @@
 #include "../include/address_map_arm.h"
 #include "../include/interrupt_ID.h"
 
-/**  your part 1 kernel code here  **/
-
-// Virtual address pointers to physical memory
-void *virtual_base;
-volatile int *key_ptr;
-volatile int *switch_ptr;
-volatile int *seven_seg_ptr;
-volatile int *seven_seg_ptr_2;
-volatile int *timer_ptr;
-
 /*
 ****************************************************
 GLOBAL VARS for virtual memory addresses to hardware
 */
-void *LW_virtual;      // used to map physical addresses for the light-weight bridge
-volatile int *KEY_ptr; // virtual address for the KEY port
-volatile int *SW_ptr;  // virtual address for the SW port
-
-volatile int KEY_state; // vars for KEY and SW values
-volatile int SW_state;
+void *virtual_base;                 // used to map physical addresses for the light-weight bridge
+volatile int *seven_seg_ptr;
+volatile int *seven_seg_ptr_2;
+volatile int *timer_ptr;
 /*
 ****************************************************
 */
-
-// Kernel functions for device driver /dev/KEY
-static int device_open_STOPWATCH(struct inode *, struct file *);
-static int device_release_STOPWATCH(struct inode *, struct file *);
-static ssize_t device_read_STOPWATCH(struct file *, char *, size_t, loff_t *);
-
-static struct file_operations chardev_STOPWATCH_fops = {
-    .owner = THIS_MODULE,
-    .read = device_read_STOPWATCH,
-    .open = device_open_STOPWATCH,
-    .release = device_release_STOPWATCH};
-
-#define DEV_NAME_STOPWATCH "stopwatch"
-#define MAX_SIZE 256
-#define SUCCESS 0
-
-// character device driver for KEY
-static struct miscdevice chardev_STOPWATCH = {
-    .minor = MISC_DYNAMIC_MINOR,
-    .name = DEV_NAME_STOPWATCH,
-    .fops = &chardev_STOPWATCH_fops,
-    .mode = 0666};
-
-static char timer_stopwatch[MAX_SIZE]; // the character array that can be read
 
 // Bit patterns to display digits 0 - 9 on HEXs
 static const char SEG[] = {
@@ -82,88 +46,36 @@ volatile int seconds_2;
 volatile int minutes;
 volatile int minutes_2;
 
-// vars for key handler
-volatile int key_pressed;
-volatile int switch_value;
-volatile int ones_column;
-volatile int tens_column;
 
-irq_handler_t key_handler(int irq, void *dev_id, struct pt_regs *regs)
-{
 
-    // check which key was pressed and current value on SWs
-    key_pressed = *(key_ptr + 3);
-    switch_value = *switch_ptr;
+// Kernel functions for device driver /dev/stopwatch
+static int device_open_STOPWATCH(struct inode *, struct file *);
+static int device_release_STOPWATCH(struct inode *, struct file *);
+static ssize_t device_read_STOPWATCH(struct file *, char *, size_t, loff_t *);
 
-    // Check which KEY was pressed, set appropriate timer value
-    if (key_pressed == 0x0001)
-    {
+static struct file_operations chardev_STOPWATCH_fops = {
+    .owner = THIS_MODULE,
+    .read = device_read_STOPWATCH,
+    .open = device_open_STOPWATCH,
+    .release = device_release_STOPWATCH};
 
-        if (!stop)
-        {
-            // stop timer
-            *(timer_ptr + 1) = 0x00000008;
-        }
-        else
-        {
-            // resume timer
-            *(timer_ptr + 1) = 0x00000007;
-        }
+#define DEV_NAME_STOPWATCH "stopwatch"
+#define MAX_SIZE 256
+#define SUCCESS 0
 
-        stop = !stop;
-    }
-    else if (key_pressed == 0x0002)
-    {
-        // set values of miliseconds, seconds, and minutes
-        // based on switch_value
-        if (switch_value <= 99)
-        {
+// character device driver for stopwatch
+static struct miscdevice chardev_STOPWATCH = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = DEV_NAME_STOPWATCH,
+    .fops = &chardev_STOPWATCH_fops,
+    .mode = 0666};
 
-            // split decimal value represented on SWs into 2 digits (ones and tens digit)
-            ones_column = switch_value % 10;
-            tens_column = switch_value / 10;
+static int chardev_stopwatch_registered = 0;
+static char timer_stopwatch[MAX_SIZE]; // the character array that can be read
 
-            miliseconds = 9 - ones_column;
-            miliseconds_2 = 9 - tens_column;
 
-            *seven_seg_ptr = (SEG[9 - miliseconds]) | (SEG[9 - miliseconds_2] << 8) | (SEG[9 - seconds] << 16) | (SEG[5 - seconds_2]) << 24;
-        }
-    }
-    else if (key_pressed == 0x0004)
-    {
 
-        if (switch_value <= 59)
-        {
-            ones_column = switch_value % 10;
-            tens_column = switch_value / 10;
-
-            seconds = 9 - ones_column;
-            seconds_2 = 5 - tens_column;
-
-            *seven_seg_ptr = (SEG[9 - miliseconds]) | (SEG[9 - miliseconds_2] << 8) | (SEG[9 - seconds] << 16) | (SEG[5 - seconds_2]) << 24;
-        }
-    }
-    else if (key_pressed == 0x0008)
-    {
-
-        if (switch_value <= 59)
-        {
-            ones_column = switch_value % 10;
-            tens_column = switch_value / 10;
-
-            minutes = 9 - ones_column;
-            minutes_2 = 5 - tens_column;
-
-            *seven_seg_ptr_2 = (SEG[9 - minutes]) | (SEG[5 - minutes_2] << 8);
-        }
-    }
-
-    // reset KEY Edge bits
-    *(key_ptr + 3) = 0xF;
-
-    return (irq_handler_t)IRQ_HANDLED;
-}
-
+// hardware timer module to keep track of time
 irq_handler_t timer_handler(int irq, void *dev_id, struct pt_regs *regs)
 {
 
@@ -220,20 +132,20 @@ irq_handler_t timer_handler(int irq, void *dev_id, struct pt_regs *regs)
 static int __init custom_init(void)
 {
 
-    // init KEY driver
-    int err_KEY = misc_register(&chardev_STOPWATCH);
-    if (err_KEY < 0)
+    // init stopwatch driver
+    int err_stopwatch = misc_register(&chardev_STOPWATCH);
+    if (err_stopwatch < 0)
     {
         printk(KERN_ERR "/dev/%s: misc_register() failed\n", DEV_NAME_STOPWATCH);
     }
     else
     {
         printk(KERN_INFO "/dev/%s driver registered\n", DEV_NAME_STOPWATCH);
+        chardev_stopwatch_registered = 1;
     }
 
-    /* initialize the message */
 
-    int value, timer_value;
+    int timer_value;
     virtual_base = ioremap(LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
 
     // Initialize time variables to 0
@@ -245,42 +157,38 @@ static int __init custom_init(void)
     minutes = 0;
     minutes_2 = 0;
 
-    // Set virtual addresses for KEYs, HEXs, SWs and timer0
-    key_ptr = virtual_base + KEY_BASE;
-    switch_ptr = virtual_base + SW_BASE;
+    // Set virtual addresses for HEXs and timer0
     seven_seg_ptr = virtual_base + HEX3_HEX0_BASE;
     seven_seg_ptr_2 = virtual_base + HEX5_HEX4_BASE;
     timer_ptr = virtual_base + TIMER0_BASE;
 
-    // Initialize timer0 and reset KEYs
+    // Initialize timer0
     *(timer_ptr + 2) = INITIAL_TIMER & 0x0000FFFF;
     *(timer_ptr + 3) = (INITIAL_TIMER & 0xFFFF0000) >> 16;
     // write 1s to Control register (START, CONT, ITO = 1) TO START
     *(timer_ptr + 1) = 0x00000007;
-    // reset KEY Edge bits
-    // set KEY interrupt bits
-    *(key_ptr + 3) = 0xF;
-    *(key_ptr + 2) = 0xF;
 
     // Set timer HEX display to start at 59:59:99
     *seven_seg_ptr = (SEG[9 - miliseconds]) | (SEG[9 - miliseconds_2] << 8) | (SEG[9 - seconds] << 16) | (SEG[5 - seconds_2]) << 24;
     *seven_seg_ptr_2 = (SEG[9 - minutes]) | (SEG[5 - minutes_2] << 8);
 
-    value = request_irq(KEY_IRQ, (irq_handler_t)key_handler,
-                        IRQF_SHARED,
-                        "button  handler",
-                        (void *)(key_handler));
-
+    
     timer_value = request_irq(TIMER0_IRQ, (irq_handler_t)timer_handler,
                               IRQF_SHARED,
                               "timer  handler",
                               (void *)(timer_handler));
 
-    return timer_value;
+
+    return err_stopwatch;
 }
 
 static int __exit custom_exit(void)
 {
+
+    if(chardev_stopwatch_registered){
+        misc_deregister(&chardev_STOPWATCH);
+        printk(KERN_INFO "/dev/%s driver de-registered\n", DEV_NAME_STOPWATCH);
+    }
 
     // Reset HEX displays and timer0
     *seven_seg_ptr = 0;
@@ -289,11 +197,10 @@ static int __exit custom_exit(void)
     *(timer_ptr + 0) = 0;
     *(timer_ptr + 3) = 0;
     *(timer_ptr + 1) = 0;
-    misc_deregister(&chardev_STOPWATCH);
-    printk(KERN_INFO "/dev/%s driver de-registered\n", DEV_NAME_STOPWATCH);
+
     iounmap(virtual_base);
     free_irq(TIMER0_IRQ, (void *)timer_handler);
-    free_irq(KEY_IRQ, (void *)key_handler);
+    
 }
 
 /*
@@ -316,9 +223,9 @@ static int device_release_STOPWATCH(struct inode *inode, struct file *file)
  * Returns, and sets *offset to, the number of bytes read. */
 static ssize_t device_read_STOPWATCH(struct file *filp, char *buffer, size_t length, loff_t *offset)
 {
-    //  *seven_seg_ptr = (SEG[9 - miliseconds]) | (SEG[9 - miliseconds_2] << 8) | (SEG[9 - seconds] << 16) | (SEG[5 - seconds_2]) << 24;
-    // *seven_seg_ptr_2 = (SEG[9 - minutes]) | (SEG[5 - minutes_2] << 8);
+    // print stopwatch time to terminal
     sprintf(timer_stopwatch, "%d%d:%d%d:%d%d\n", 5 - minutes_2, 9 - minutes, 5 - seconds_2, 9 - seconds, 9 - miliseconds_2, 9 - miliseconds);
+    
     size_t bytes;
     bytes = strlen(timer_stopwatch) - (*offset); // how many bytes not yet sent?
     bytes = bytes > length ? length : bytes;     // too much to send all at once?
