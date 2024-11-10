4fc6de9267b90fe9a2df65faaeb9e464c6cb86c9
bigwhoman <hm.keshvari@gmail.com>
2024-11-10 12:18:26 -0500

    fixed bugs and completed the driver

--- E6/fpga/e6_template/part1/video.c
+++ E6/fpga/e6_template/part1/video.c
@@ -11,8 +11,6 @@
 /**  your part 1 kernel code here  **/
 
 
-
-
 void *LW_virtual;   // used to access FPGA light-weight bridge
 volatile int * pixel_ctrl_ptr; // virtual address of pixel buffer controller
 int * pixel_buffer;   // used for virtual address of pixel buffer
@@ -53,13 +51,11 @@ static char chardev_read[MAX_SIZE];     // the character array that would be wri
 /* Code to initialize the video driver */
 static int __init start_video(void){
     // initialize the miscdevice data structures
-    /* TODO */
     int err_video = misc_register (&chardev_video);
     if (err_video < 0) {
         printk (KERN_ERR "/dev/%s: misc_register() failed\n", DEV_NAME_VIDEO);
     } else {
         printk (KERN_INFO "/dev/%s driver registered\n", DEV_NAME_VIDEO);
-        // chardev_LEDR_registered = 1;
     }
 
 
@@ -76,7 +72,6 @@ static int __init start_video(void){
     pixel_buffer = (int *)ioremap_nocache(FPGA_ONCHIP_BASE, FPGA_ONCHIP_SPAN);
     if (pixel_buffer == 0)
         printk(KERN_ERR "Error: ioremap_nocache returned NULL\n");
-    printk(KERN_INFO "%p -a-a-a--a \n", pixel_buffer);
     
     /* Erase the pixel buffer */
     clear_screen();
@@ -84,33 +79,36 @@ static int __init start_video(void){
 }
 
 void get_screen_specs(volatile int * pixel_ctrl_ptr){
-    /* TODO */
-    printk(KERN_INFO "----------> %X\n", *(pixel_ctrl_ptr + 2));
     resolution_x = (*(pixel_ctrl_ptr + 2)) & 0xFFFF;
     resolution_y = ((*(pixel_ctrl_ptr + 2)) & 0xFFFF0000) >> 16;
-    printk(KERN_INFO " : %x -- : %x\n",*(pixel_ctrl_ptr), *(pixel_ctrl_ptr + 1));
 }
 
+/*
+*   Writes 0x0 (RGB Black) into 
+*   all pixels of VGA
+*/
 void clear_screen(){
-    /* TODO */
+    size_t x, y;
+    for ( x = 0; x <= resolution_x; x++)
+    {
+        for ( y = 0; y <= resolution_y; y++)
+        {
+            plot_pixel(x, y, 0x0);
+        }
+    }
 }
 
 void plot_pixel(int x, int y, short int color){
-    /* TODO */
-    // *(pixel_buffer +  (((x) | ((y << 9))) << 1 )/4) = color;
-    // printk(KERN_INFO "%X , %X , %X , %X, %X, %X\n", x, y, (x & 0x1FF) , (y & 0xFF), ((y & 0xFF) << 9), ((x & 0x1FF) | ((y & 0xFF) << 9)));
-    // printk(KERN_INFO "%X , %X , %X\n", pixel_buffer ,  (((x & 0x1FF) | ((y & 0xFF) << 9)) << 1 )/4,(int *)((void *)pixel_buffer + (((x & 0x1FF) | ((y& 0xFF) << 9)) << 1 )));
-    // *pixel_buffer = color;
-    // *(int *)((void *)pixel_buffer + (((x & 0x1FF) | ((y& 0xFF) << 9)) << 1 )) = color;
+    *(short int *)((void *)pixel_buffer + (((x & 0x1FF) | ((y& 0xFF) << 9)) << 1 )) = color;
 }
 
 static void __exit stop_video(void){
+    clear_screen();
     /* unmap the physical-to-virtual mappings */
     iounmap (LW_virtual);
     iounmap ((void *) pixel_buffer);
 
     /* Remove the device from the kernel */
-    /* TODO */
     misc_deregister(&chardev_video);
     printk(KERN_INFO "/dev/%s driver de-registered\n", DEV_NAME_VIDEO);
 }
@@ -141,7 +139,6 @@ static ssize_t device_read(struct file *filp, char* buffer,
 
 static ssize_t device_write(struct file *filp, const char 
     *buffer, size_t length, loff_t *offset) {
-    // static char str_value_input[7];
     size_t bytes;
     bytes = length;
     int x,y;
@@ -158,24 +155,14 @@ static ssize_t device_write(struct file *filp, const char
     */
     if (!strncmp(chardev_video_msg, "clear", 5)){
         printk(KERN_INFO "Clear Screen !!!!\n");
-        /* TODO */
-    } else if (sscanf(chardev_video_msg, "pixel %d,%d %X", &x, &y, &color ) == 3) {
-        // printk(KERN_INFO "Color Pixel : %d,%d ---> %X\n", x, y , color);
-        *(int *)((void *)pixel_buffer + (((x & 0x1FF) | ((y& 0xFF) << 9)) << 1 )) = color;
-        /* TODO */
+        clear_screen();
+    } else if (sscanf(chardev_video_msg, "pixel %d,%d %hX", &x, &y, &color) == 3) {
+        plot_pixel(x, y, color);
+
+
     } else {
         printk(KERN_ERR "Wrong Command\n");
-    }                 
-    // int string_length = strlen(str_value_input);
-
-
-    //DOUBLE CHECK FOR VALID INPUT: EXPECTS 3 DIGIT HEX INPUT
-    // if( strlen(chardev_LEDR_msg) == 4 && !not_hex && ledr_value >= 0x000 && ledr_value <= 0x3FF ){
-    //     *LEDR_ptr = ledr_value;                       //Display value on LEDR
-    // }
-    // else{
-    //     printk (KERN_ERR "Bad argument for /dev/LEDR %s", chardev_LEDR_msg);
-    // }
+    }
 
     return bytes;
 }
